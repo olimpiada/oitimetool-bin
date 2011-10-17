3bf124f4417b0a59fea08122a9feb7cf725f5a0c
Szymon Acedański <accek@mimuw.edu.pl>
2011-10-17 22:24:48 +0200

    updated to PinTools 2.10-43611

--- oitimetool.bat
+++ oitimetool.bat
@@ -1,11 +1,11 @@
 @echo off
 set PIN_DIR0=%~dp0
-set PIN_DIR=%PIN_DIR0%\pin-2.10-41150-msvc10-ia32_intel64-windows
+set PIN_DIR=%PIN_DIR0%\pin-2.10-43611-msvc10-ia32_intel64-windows
 set PIN_ZIP=%PIN_DIR%.zip
 
 if exist %PIN_DIR% goto found
 if exist %PIN_ZIP% goto have_zip
-echo Please download PinTool library from http://tiny.pl/h5gc4
+echo Please download PinTool library from http://tiny.pl/h1f3k
 echo and save the ZIP file to %PIN_DIR0%.
 exit /b 1
 
